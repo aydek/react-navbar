7b23542d30683344d6f54b07bf04272e9d7bcb5b
aydek <kesa0915@hotmail.com>
2022-03-11 01:48:17 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
     };
     return (
         <div className="nav_window">
-        
             <div className="nav_body">
                 <div className={`item_body ${navIndex === 0 ? 'active' : ''}`} onClick={setIndex(0)}>
                     <p>Home</p>
